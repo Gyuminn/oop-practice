8dd6f95e0287b6d5bcdcf35631ff215fe920015c
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-10-21 17:15:02 +0900

    비밀번호 유효성 검증기 세팅

---
+++ src/test/java/org/example/UserTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @DisplayName("패스워드를 초기화 한다")
+    @Test
+    void passwordTest() {
+
+    }
+}
