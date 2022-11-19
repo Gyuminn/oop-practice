9a061c92f2340bab737371ec7976ef40a3978c96
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-11-19 11:14:38 +0900

    (refactor: Calculator) make new Interface, stop use Enum, solve Exception handling

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,8 +1,24 @@
 package org.example;
 
+import org.example.calculate.*;
+
+import java.util.List;
+
 public class Calculator {
-    // 2. calculate 안에서 수행할 수도 있지만 ArithmeticOperator를 만들어서 해당 ENUM에게 다시 한 번 작업을 위침한다.
+    // 4. 각각의 구현체들을 인터페이스인 NewArithmerticOperator를 통해서 받는다. 더 이상 ENUM 사용 x
+    private static final List<NewArithmeticOperator> arthemeticOperators = List.of(new AdditionOperator(), new SubtractionOperator(), new MultiplicationOperator(), new DivisionOperator());
+
     public static int calculate(int operand1, String operator, int operand2) {
-        return ArithmeticOperator.calculate(operand1, operator, operand2);
+        return arthemeticOperators.stream()
+                .filter(arthemeticOperator -> arthemeticOperator.supports(operator))
+                .map(arthemeticOperator -> arthemeticOperator.calculate(operand1, operand2))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("올바른 사칙연산이 아닙니다."));
     }
+
+    // 2. calculate 안에서 수행할 수도 있지만 ArithmeticOperator를 만들어서 해당 ENUM에게 다시 한 번 작업을 위임한다.
+    // public static int calculate(int operand1, String operator, int operand2) {
+    // return ArithmeticOperator.calculate(operand1, operator, operand2);
+    // }
+
 }
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -9,13 +10,14 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 /**
- * 간단한 사칙연산을 할 수 있다.
- * 양수로만 계산할 수 있다.
- * 나눗셈에서 0을 나누는 경우 IllegalArgument 예외를 발생시킨다.
- * MVC패턴(Model-View-Controller) 기반으로 구현한다.
+ * [o] 간단한 사칙연산을 할 수 있다.
+ * [ ] 양수로만 계산할 수 있다.
+ * [o] 나눗셈에서 0으로 나누는 경우 IllegalArgument 예외를 발생시킨다.
+ * [ ] MVC패턴(Model-View-Controller) 기반으로 구현한다.
  */
 
 public class CalculatorTest {
@@ -39,5 +41,13 @@ public class CalculatorTest {
                 arguments(4, "*", 2, 8),
                 arguments(4, "/", 2, 2)
         );
+    }
+
+    @DisplayName("나눗셈에서 0으로 나누는 경우 IllegalArgument 예외를 발생시킨다")
+    @Test
+    void calculateExceptionTest() {
+        assertThatCode(() -> Calculator.calculate(10, "/", 0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("0으로는 나눌 수 없습니다");
     }
 }
